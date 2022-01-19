94ad3af8ae39528d41bf597ddef920fb377129ef
roystoliday <roystoliday@hotmail.com>
2022-01-19 10:42:59 +0000

    Excel communication
    Now talks to the excel sheet.
    You can write to the log, and make cells yellow.

--- excel-vue-plugin/starcount-vue/src/components/HelloWorld.vue
+++ excel-vue-plugin/starcount-vue/src/components/HelloWorld.vue
@@ -1,15 +1,42 @@
 <template>
 <div class="container">
   <h3>Hello World!</h3>
-  <p>I'm from a component in Vue.</p>
-  <p>But, still no cheddar.  This can't be allowed to persist.</p>
+  <p>I'm from a component in Vue!</p>
+  <p>But, still no cheddar.</p>
+  <button @click="run()">Make cells yellow</button>
+  <button @click="writeToLog()">Write to log</button>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  methods: {
+    async run() {
+      await Excel.run(async (context) => {
+        const range = context.workbook.getSelectedRange();
+        range.format.fill.color = "yellow";
+        range.load("address");
+
+        await context.sync();
+
+        console.log(`The range address was "${range.address}".`);
+      });
+    },
+    async tryCatch(callback) {
+      try {
+        await callback();
+      } catch (error) {
+        // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+        console.error(error);
+      }
+    },
+    writeToLog() {
+      console.log('From HelloWorld, Hello.');
+    }
+  }
 }
+
 </script>
 
 <style scoped>
